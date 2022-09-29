a80e68f6c268d96f75e58b94a0939ecec550702c
HarshitBhargava412 <harshitbhargava042001@gmail.com>
2022-09-29 17:14:00 +0530

    change

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -8,6 +8,43 @@ function Services() {
         <div className='services-container-head'>
             SERVICES
         </div> 
+        <div className='services-container-sub-head'>
+            WHAT CAN WE HELP YOU WITH ?
+        </div>
+        <div className='services-icons-container'>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/sofa.png")} alt="icon" />
+                <div className='services-icon-name'>Styling & Decor</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/building.png")} alt="icon" />
+                <div className='services-icon-name'>Residential Design</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/house.png")} alt="icon" />
+                <div className='services-icon-name'>Commercial Design</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/house.png")} alt="icon" />
+                <div className='services-icon-name'>Commercial Design</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/headphones.png")} alt="icon" />
+                <div className='services-icon-name'>Consultation</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/scale.png")} alt="icon" />
+                <div className='services-icon-name'>Styling & Decor</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/chair.png")} alt="icon" />
+                <div className='services-icon-name'>Styling & Decor</div>
+            </div>
+            <div className='services-icon-container'>
+                <img src={require("../../images/services-icons/house.png")} alt="icon" />
+                <div className='services-icon-name'>Commercial Design</div>
+            </div>
+        </div>
       </div>
     </div>
   )
